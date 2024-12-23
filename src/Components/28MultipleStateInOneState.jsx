a1952dca114a944b5842568e896b5cb6bae659dd
tharuAnish <namoanishtharu@gmail.com>
2024-12-23 22:00:25 +0530

    Multi state in one state completed

--- src/Components/28MultipleStateInOneState.jsx
+++ src/Components/28MultipleStateInOneState.jsx
@@ -1,12 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 
 const MultipleStateInOneState = () => {
+  const [state, setState] = useState({
+    count: 0,
+    text: "Hello World!",
+    isActive: true,
+  })
+
+  function incrementCount() {
+    setState({ ...state, count: state.count + 1 })
+  }
+  function changeText() {
+    setState({ ...state, text: "Changed Text" })
+  }
+
+  function toggleStatus() {
+    setState({ ...state, isActive: !state.isActive })
+  }
+
   return (
     <div>
       <h4>MultipleStateInOneState</h4>
-      <p>Count:</p>
-      <p>Text:</p>
-      <p>Active:</p>
+      <p>Count:{state.count}</p>
+      <p>Text:{state.text}</p>
+      <p>{state.isActive ? "Active" : "NotActive"}</p>
+
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={changeText}>Change Text</button>
+      <button onClick={toggleStatus}>Toggle Status</button>
     </div>
   )
 }
